29dccb9c86e3e4b3488dc37fc30ec94bedd8f9b2
AmreenTester <amreen104026@gmail.com>
2026-07-19 16:12:54 +0530

    remove terraform check syntax

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app_server" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   subnet_id              = var.subnet_id
   vpc_security_group_ids = ["sg-05c25390874fc7fab"]
