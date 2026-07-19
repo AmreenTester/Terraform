e3a1a5ef680bc2c97b05e1ca53a6955a9e5e10bb
AmreenTester <amreen104026@gmail.com>
2026-07-19 16:35:05 +0530

    storing state on remote and download artifact

--- provider.tf
+++ provider.tf
@@ -1,13 +1,6 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-provider "aws" {
-  alias  = "south"
-  region = "ap-south-1"
-}
-
 terraform {
+  required_version = ">= 1.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,7 +8,18 @@ terraform {
     }
   }
 
-  required_version = ">= 1.2"
+  backend "s3" {
+    bucket  = "terraform19july2026"
+    key     = "ec2/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
 }
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
 
 
